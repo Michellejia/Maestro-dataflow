25bc53875a2a2db2e97ee712795af98083cce9ce
Yuwen Jia <michelle2010tx@hotmail.com>
2023-03-23 23:22:46 -0700

    add best dataflows

---
+++ artifacts/os.m
@@ -0,0 +1,9 @@
+Dataflow { 
+        TemporalMap(Sz(S),1) X;
+        SpatialMap(Sz(R),1) Y;
+        Cluster(256, P);
+        SpatialMap(1, 1) K;
+        TemporalMap(1, 1) C;
+        TemporalMap(Sz(R),Sz(R)) R;
+        TemporalMap(Sz(S),Sz(S)) S;
+} 
